b738bf38104ed05757406a50594bada9ec0cbd7f
c-atfish <jonlar@student.kristiania.no>
2022-02-11 14:59:28 +0100

    added index page

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
@@ -11,6 +12,10 @@ app.post("/api/login", (req, res, next) => {
 });
 
 app.use(express.static("../client/dist"));
+
+app.use((req, res) => {
+  res.sendFile(path.resolve("..client"));
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
