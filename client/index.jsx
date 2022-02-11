e3a942af1acf93a587f5b317f8142eabbd026f16
c-atfish <jonlar@student.kristiania.no>
2022-02-11 14:40:21 +0100

    added index page

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Application() {
+  return (
+    <div>
+      <h2>My react app</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
